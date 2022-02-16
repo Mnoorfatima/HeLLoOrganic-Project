26d71e4a5b31661b8cef99cc7e3fe0992bd43faf
helloorganic <mahnoorkhurram645@gmail.com>
2022-02-16 01:26:26 -0800

    signfragment

---
+++ app/src/main/java/com/example/helloorganic/login_fragment.java
@@ -0,0 +1,20 @@
+package com.example.helloorganic;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class login_fragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_fragment, container, false);
+    }
+}
