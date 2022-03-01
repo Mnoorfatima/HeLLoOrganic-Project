9aae55b820e56e44e508117bd125dd013054f563
helloorganic <mahnoorkhurram645@gmail.com>
2022-02-28 19:41:33 -0800

    Activites

---
+++ app/src/main/java/com/example/helloorganic/about_us.java
@@ -0,0 +1,14 @@
+package com.example.helloorganic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class about_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
---
+++ app/src/main/java/com/example/helloorganic/vist_us.java
@@ -0,0 +1,14 @@
+package com.example.helloorganic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vist_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vist_us);
+    }
+}
