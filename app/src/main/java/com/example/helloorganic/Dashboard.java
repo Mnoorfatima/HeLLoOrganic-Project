63a9ef909fb35b4c69681773481004ac298be4a9
Mnoorfatima <noorr76noor@gmail.com>
2022-02-28 20:08:56 -0800

    Activites Java

--- app/src/main/java/com/example/helloorganic/Dashboard.java
+++ app/src/main/java/com/example/helloorganic/Dashboard.java
@@ -1,14 +1,72 @@
 package com.example.helloorganic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
 
+    CardView c1,c2,c3,c4,c5,c6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        c1=findViewById(R.id.c1);
+        c2=findViewById(R.id.c2);
+        c3=findViewById(R.id.c3);
+        c4=findViewById(R.id.c4);
+        c5=findViewById(R.id.c5);
+        c6=findViewById(R.id.c6);
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,vist_us.class);
+                startActivity(intent);
+            }
+        });
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,shope.class);
+                startActivity(intent);
+
+            }
+        });
+
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,favourite.class);
+                startActivity(intent);
+            }
+        });
+        c4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,contact_us.class);
+                startActivity(intent);
+            }
+        });
+        c5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,about_us.class);
+                startActivity(intent);
+            }
+        });
+        c6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,profile.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
 }
